e12f770195406c2100b4403d997c37050e5fa68a
shweta1112 <shweta.cs0927@gmail.com>
2019-10-01 12:27:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import InfiniteScroll from "react-infinite-scroller";
+
+class Scroll2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: 20,
+      hasMoreItems: true
+    };
+  }
+
+  showItems() {
+    var items = [];
+    for (var i = 0; i < this.state.items; i++) {
+      items.push(<li key={i}> Item {i} </li>);
+    }
+    return items;
+  }
+
+  loadMore() {
+    if (this.state.items === 200) {
+      this.setState({ hasMoreItems: false });
+    } else {
+      setTimeout(() => {
+        this.setState({ items: this.state.items + 20 });
+      }, 2000);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title"> Welcome to React </h1>{" "}
+        </header>
+        <div style={{ height: "200px", overflow: "auto" }}>
+          <InfiniteScroll
+            loadMore={this.loadMore.bind(this)}
+            hasMore={this.state.hasMoreItems}
+            loader={<div className="loader"> Loading... </div>}
+            useWindow={false}
+          >
+            {this.showItems()}{" "}
+          </InfiniteScroll>{" "}
+        </div>{" "}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Scroll2;
