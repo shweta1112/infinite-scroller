a9cad4cf081638f0b2141cba93cda77a5fb992bb
shweta1112 <shweta.cs0927@gmail.com>
2019-10-01 13:12:27 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class Scroll2 extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: 20,
+      items: 1000,
       hasMoreItems: true
     };
   }
@@ -26,8 +26,8 @@ class Scroll2 extends Component {
       this.setState({ hasMoreItems: false });
     } else {
       setTimeout(() => {
-        this.setState({ items: this.state.items + 20 });
-      }, 2000);
+        this.setState({ items: this.state.items + 1000 });
+      }, 1000);
     }
   }
 
@@ -38,7 +38,7 @@ class Scroll2 extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title"> Welcome to React </h1>{" "}
         </header>
-        <div style={{ height: "200px", overflow: "auto" }}>
+        <div style={{ height: "1000px", overflow: "auto" }}>
           <InfiniteScroll
             loadMore={this.loadMore.bind(this)}
             hasMore={this.state.hasMoreItems}
